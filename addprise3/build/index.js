9ef3bfc80fa2193f6567d118f96aa1297a488f09
yhlmmm <yinhengli@sina.com>
2017-09-18 17:04:22 +0800

    webpack打包前端js文件， webpack-dev-server添加，但启动失败

---
+++ addprise3/build/index.js
@@ -0,0 +1,153 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+//父类，点赞后count+1
+var PraiseButton = function () {
+	function PraiseButton(element, limit, callback) {
+		_classCallCheck(this, PraiseButton);
+
+		this.count = 0;
+		this.element = element;
+		this.limit = limit;
+		this.callback = callback;
+		this.clickElement();
+	}
+
+	_createClass(PraiseButton, [{
+		key: 'addPraise',
+		value: function addPraise() {
+			this.count = addOne(this.count);
+		}
+	}, {
+		key: 'clickElement',
+		value: function clickElement() {
+			var _this = this;
+			var op = function op() {
+				if (_this.count + 1 >= _this.limit) {
+					//变为灰色
+					$(this).css('filter', 'grayscale(100%)');
+					$(this).unbind("click");
+				}
+				_this.addPraise();
+				_this.showAnimate();
+				_this.callback();
+			};
+			//绑定节流函数
+			var createThrottle = throttle(op, 1000);
+			$(this.element).find('.hand').click(function () {
+				createThrottle();
+			});
+		}
+	}, {
+		key: 'showAnimate',
+		value: function showAnimate() {
+			var addone = $('<div class="hand-small">+' + this.count + '</div>');
+			$(this.element).find('.hand').append(addone);
+			$(addone).addClass('animate');
+		}
+	}]);
+
+	return PraiseButton;
+}();
+
+var Thumb = function (_PraiseButton) {
+	_inherits(Thumb, _PraiseButton);
+
+	function Thumb(_ref) {
+		var element = _ref.element,
+		    limit = _ref.limit,
+		    callback = _ref.callback;
+
+		_classCallCheck(this, Thumb);
+
+		return _possibleConstructorReturn(this, (Thumb.__proto__ || Object.getPrototypeOf(Thumb)).call(this, element, limit, callback));
+	}
+
+	return Thumb;
+}(PraiseButton);
+
+exports.default = Thumb;
+
+/***/ })
+/******/ ]);
--- addprise3/webpack.config.js
+++ addprise3/webpack.config.js
@@ -1,12 +1,28 @@
+var path=require('path');
 module.exports = {
-    entry: "./app.es",
+    entry: [
+        './public/scripts/index.js',
+        './public/stylesheets/index.css',
+        './public/scripts/addone.js',
+        './public/scripts/throttle.js'
+    ],
     output: {
-        path: __dirname+'/build',
-        filename: "bundle.js"
+        path: __dirname + '/public/scripts/',
+        filename: "[name].js"
     },
     module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
+        loaders: [{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }, {
+            test: /\.js$/,
+            loader: "babel-loader"
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        compress: true,
+        port: 5000,
+        hot: true
     }
-};
+}
